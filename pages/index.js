f74ca3b3cf3c2e7e464b25013ff60e711e9ecde7
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-10-30 17:56:44 +0300

    banner-carousel

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+$(function() {
+
+    var owl=$(".banner-carousel__carousel");
+    owl.owlCarousel({
+        loop: true,
+        responsive:{
+            0: {
+                items: 1
+            }
+        }
+    });
+
+    //$(".next") - находим нашу кнопку
+    $(".banner-carousel__carousel-button_type_next").click(function(){
+        owl.trigger("next.owl.carousel");
+    });
+    $(".banner-carousel__carousel-button_type_prev").click(function(){
+        owl.trigger("prev.owl.carousel");
+    });
+});
 
 
 
