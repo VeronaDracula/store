e2fa90c0272c9cf33d1d9c7d40f3775ef25f35a9
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-10-31 15:09:58 +0300

    our-products

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 $(function() {
-
     var owl=$(".banner-carousel__carousel");
     owl.owlCarousel({
         loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
         responsive:{
             0: {
                 items: 1
@@ -15,6 +16,27 @@ $(function() {
         owl.trigger("next.owl.carousel");
     });
     $(".banner-carousel__carousel-button_type_prev").click(function(){
+        owl.trigger("prev.owl.carousel");
+    });
+});
+
+
+$(function() {
+    var owl=$(".products__carousel");
+    owl.owlCarousel({
+        rewind:true,
+        responsive:{
+            0: {
+                items: 5
+            }
+        }
+    });
+
+    //$(".next") - находим нашу кнопку
+    $(".products__carousel-button_type_next").click(function(){
+        owl.trigger("next.owl.carousel");
+    });
+    $(".products__carousel-button_type_prev").click(function(){
         owl.trigger("prev.owl.carousel");
     });
 });
